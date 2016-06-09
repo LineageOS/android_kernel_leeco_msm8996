c1976b8ea172f58fe8f2b3f8af9b9044b47531aa
Srinath Sridharan <srinathsr@google.com>
2016-06-09 15:39:31 -0700

    Fixing compilation errors in fair.c and core.c due to merging EAS patches into mainline android-msm-marlin-3.18.
    Change-Id: I8b9b1113a1138183c8bd364f3d2dd88dc156f100

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -820,6 +820,23 @@ void sched_avg_update(struct rq *rq)
 	}
 }
 
+/*
+ * Note C-state for (idle) cpus.
+ *
+ * @cstate = cstate index, 0 -> active state
+ * @wakeup_energy = energy spent in waking up cpu
+ * @wakeup_latency = latency to wakeup from cstate
+ *
+ */
+void
+sched_set_cpu_cstate(int cpu, int cstate, int wakeup_energy, int wakeup_latency)
+{
+	struct rq *rq = cpu_rq(cpu);
+
+	rq->cstate = cstate; /* C1, C2 etc */
+	rq->wakeup_energy = wakeup_energy;
+	rq->wakeup_latency = wakeup_latency;
+}
 #if defined(CONFIG_RT_GROUP_SCHED) || (defined(CONFIG_FAIR_GROUP_SCHED) && \
 			(defined(CONFIG_SMP) || defined(CONFIG_CFS_BANDWIDTH)))
 /*
