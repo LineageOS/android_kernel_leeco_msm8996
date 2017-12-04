69f736f5ceef8839bacf8af57619bffd671b9d7c
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-12-04 19:55:16 +0100

    mdss: import livedisplay driver from cm-14.1
    Change-Id: I7b2f3aa38c353a720361dc04180c0ca32ad1f8d6

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -55,6 +55,7 @@
 #include "mdss_smmu.h"
 #include "mdss_mdp.h"
 #include "mdp3_ctrl.h"
+#include "mdss_livedisplay.h"
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
@@ -930,6 +931,11 @@ static int mdss_fb_create_sysfs(struct msm_fb_data_type *mfd)
 	rc = sysfs_create_group(&mfd->fbi->dev->kobj, &mdss_fb_attr_group);
 	if (rc)
 		pr_err("sysfs group creation failed, rc=%d\n", rc);
+
+	rc = mdss_livedisplay_create_sysfs(mfd);
+	if (rc)
+		pr_err("livedisplay sysfs group creation failed, rc=%d\n", rc);
+
 	return rc;
 }
 
@@ -5110,3 +5116,4 @@ void mdss_fb_calc_fps(struct msm_fb_data_type *mfd)
 		mfd->fps_info.frame_count = 0;
 	}
 }
+
