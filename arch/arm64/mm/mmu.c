x2-new/kernel_leeco_msm8996

mmu.c: Fix ERROR_ON_MISMATCH

arch/arm64/mm/mmu.c
@@ -277,7 +277,7 @@ static inline bool use_1G_block(unsigned long addr, unsigned long next,
 	return true;
 }
 
-static void alloc_init_pud(struct mm_struct *mm, pgd_t *pgd,
+static void __init alloc_init_pud(struct mm_struct *mm, pgd_t *pgd,
 				  unsigned long addr, unsigned long end,
 				  phys_addr_t phys, pgprot_t prot,
 				  void *(*alloc)(unsigned long size), bool force_pages)

LineageOS/android_kernel_leeco_msm8996

Change-Id: I163171867bad8d0692a291e11439e27168f3ad41
Signed-off-by: celtare21 <celtare21@gmail.com>

arch/arm64/mm/mmu.c
@@ -277,7 +277,7 @@ static inline bool use_1G_block(unsigned long addr, unsigned long next,
 	return true;
 }
 
-static void alloc_init_pud(struct mm_struct *mm, pgd_t *pgd,
+static void __ref alloc_init_pud(struct mm_struct *mm, pgd_t *pgd,
 				  unsigned long addr, unsigned long end,
 				  phys_addr_t phys, pgprot_t prot,
 				  void *(*alloc)(unsigned long size), bool force_pages)