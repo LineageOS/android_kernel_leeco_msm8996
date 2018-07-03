57313260423b839759abf3e5299e860730455406
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2018-07-03 14:38:01 +0200

    Normal charging: use lower ampere
    This partially reverts Change-Id: I924ad2bf8c17ea28c90cb3dc65e28e035398d6b5
    
    The UI shows "fast charging" all the time with the old values no matter if I use a QC3.0 or 2A/5V charger.
    This should not happen to avoid confusion on user side. If the user wants fast charging, he can opt for a QC3.0 charger.
    
    Change-Id: I3b14fa8fbb3175093991905f678990f66e51f33e

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -511,7 +511,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_dcp_icl_ma = 2000;
+static int smbchg_default_dcp_icl_ma = 1800;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
